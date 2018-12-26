d087d29cf9c074174e8e37b11797095ce6fe8c8f
deloff <patch419@gmail.com>
2018-12-26 18:09:52 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Root () {
+  return (
+    <div>Helloworld</div>
+  )
+}
+
+ReactDOM.render(
+  <Root />, 
+  document.getElementById('root')
+);
